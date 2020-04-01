1f21af3600255bd9190bee58a79820f1393cc4d2
Tianna Vazquez <tiannavazquez@gmail.com>
2020-04-01 17:14:23 -0400

    Adding exceptions

--- generate.py
+++ generate.py
@@ -12,13 +12,15 @@ def main():
     """Generate 100 prime numbers and output it into output.csv file
  
     """
-    primes = getNPrime(100)
+    try:
+        primes = getNPrime(100)
+        write_primes(primes, 'output.csv')
 
-    write_primes(primes, 'output.csv')
-
-    l = read_primes('output.csv')
-
-    print(l)
+        l = read_primes('output.csv')
+    except IOError:
+        print("File cannot be found")
+    else:
+        print(l)
 
 
 if __name__ == '__main__':
--- primepackage/primemodule.py
+++ primepackage/primemodule.py
@@ -2,20 +2,26 @@
 
 import math
 """ Determine if a number is prime or not. 
+    
     Arguments: 
-    n (integer): Natural number
+        n (integer): Natural number
+    
     Return: 
-    Boolean: True if n is prime, false if not.
+        Boolean: True if n is prime, false if not.
+    Raises:
+        Exception if n is not greater than 0
 """
 def isPrime(n):
-    if n > 1:
-        for i in range(2, int(math.sqrt(n))+1):
+    if n <= 0:
+        raise ValueError("Value must be greater than 0")
+    if n > 0:
+        if n == 2:
+            return True
+        for i in range(2, n):
             if (n % i) == 0:
-                return True
+                return False
+        return True
 
-            return False
-
-    return False
 def getNPrime(num):
     """ Generates a list of prime numbers. Amount of numbers is given in parameter.
         Arguments:
